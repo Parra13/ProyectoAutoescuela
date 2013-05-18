7bf382c3ba16205d766906da0542d02c1c786e44
Rodrigo Parra Nicolau <rodri.parra13@gmail.com>
2013-05-18 18:59:03 +0200

    Añadidos comentarios y inicialización CSS
    Añadidos comentarios aclarativos sobre el funcionamiento y
    estructuración del código. Inicio de la estructura estética de la
    página.

--- operativa.js
+++ operativa.js
@@ -1,5 +1,7 @@
  $(document).ready(function () {
-
+    
+    var arraydatos = new Array(); //Inicializamos el array para que sea una variable global y no haya problemas a la hora de usarla en diferentes funciones.
+    
     $.ajax({
         type: "GET",
         url: 'preguntas y respuestas.xml', //nombre del archivo a leer.
@@ -8,15 +10,18 @@
         error: function () { alert("Error: algo ha ido mal."); }
     });    
 
-    function parse(document) {
-               
-        var arraydatos = new Array();               
+    function parse(document) {           
 
         arraydatos = $(document).find("pregunta");
-
+        var h = 1;
+        
         for (var i = 0; i < arraydatos.length; i++) {
-                   
+            
             $("#content").append(
+                
+                '<div id="Pregunta'+ h +'">' +
+                
+                '<b>Pregunta '+h+'</b>' + '</p>' +
 
                	'<br/> Enunciado: ' + $(arraydatos[i]).find(':first-child').text() +
                              
@@ -27,54 +32,52 @@
 		      	'<input id="B" class='+ $(arraydatos[i]).find('#b').attr("class") +' name='+ $(arraydatos[i]).find('#b').attr("nombre") +' type="radio" />' + 
                                         
                 '<br /> C .- ' + $(arraydatos[i]).find('#c').text() +
-           		'<input id="C" class='+ $(arraydatos[i]).find('#c').attr("class") +' name='+ $(arraydatos[i]).find('#c').attr("nombre") +' type="radio" />' + '</p>');
+           		'<input id="C" class='+ $(arraydatos[i]).find('#c').attr("class") +' name='+ $(arraydatos[i]).find('#c').attr("nombre") +' type="radio" />' + '</p>' +
+                '</div>');
+            h++;
         };
         
-        /*He tenido que incluir dentro de la función "Parse" la función que permite corregir respuestas, ya que para hacer el "for" necesitamos la longitud
-          del array para indicar cuando ha de parar de corregir. Al ser una variable local, si ubicamos la función "corrección" fuera de la funcion "Parse",
-          el array no estará declarada y dará error.*/
+    };
 
-        $("#Submit").click(function correccion() { //La función se disparará al hacer click en el boton "enviar", y se encargará de cotejar las respuestas
+    $("#Submit").click(function correccion() { //La función se disparará al hacer click en el boton "enviar", y se encargará de cotejar las respuestas
                                                    //seleccionadas. Si la respuesta es correcta aumentará el contador, sino pasará a la siguiente.
-            
-            var ContadorCorrectas = 0;
+        var ContadorCorrectas = 0;
 
-            for (var i = 1; i <= arraydatos.length ; i++) { 
-                    
-                var resultadoA = $('#A[name=Pregunta'+i+']:checked').val(); /*Captura el atributo checked del radio button de nombre RespA*/
-                var resultadoB = $('#B[name=Pregunta'+i+']:checked').val(); /*Captura el atributo checked del radio button de nombre RespB*/
-                var resultadoC = $('#C[name=Pregunta'+i+']:checked').val(); /*Captura el atributo checked del radio button de nombre RespC*/
+        for (var i = 1; i <= arraydatos.length ; i++) { 
+            /* Captura en una variable si el atributo está "on" */       
+            var resultadoA = $('#A[name=Pregunta'+i+']:checked').val(); /*Captura si el atributo checked del radio button de nombre Pregunta + nº está activado*/
+            var resultadoB = $('#B[name=Pregunta'+i+']:checked').val(); /*Captura si el atributo checked del radio button de nombre Pregunta + nº está activado*/
+            var resultadoC = $('#C[name=Pregunta'+i+']:checked').val(); /*Captura si el atributo checked del radio button de nombre Pregunta + nº está activado*/
 
-                if (resultadoA == 'on') {      //Si el valor capturado en la variable antes declarada es "on" se realiza el código dentro de llaves.
+            if (resultadoA == 'on') {      //Si el valor capturado en la variable antes declarada es "on" se realiza el código dentro de llaves.
                     
-                    var TipoRespuesta = $("#A").attr("class");
-                    
-                    if (TipoRespuesta == "Correcta") {
-                        ContadorCorrectas++;
-                    };
+                var TipoRespuesta = $("#A").attr("class"); //Se captura, en la variable, la class de la pregunta correspondiente en el XML.
+                   
+                if (TipoRespuesta == "Correcta") { //Si el valor capturado corresponde a respuesta "correcta" se incrementa en 1 el contador de respuestas. 
+                    ContadorCorrectas++;
                 };
-                
-                if (resultadoB == 'on') {      //Si el valor capturado en la variable antes declarada es "on" se realiza el código dentro de llaves.
-                    
-                    var TipoRespuesta = $("#B").attr("class");
-                    
-                    if (TipoRespuesta == "Correcta") {
-                        ContadorCorrectas++;
-                    };
-                }; 
-                                
-                if (resultadoC == 'on') {     //Si el valor capturado en la variable antes declarada es "on" se realiza el código dentro de llaves.
-                    
-                    var TipoRespuesta = $("#C").attr("class");
-                    
-                    if (TipoRespuesta == "Correcta") {
-                        ContadorCorrectas++;
-                    };
-                };    
             };
+                
+            if (resultadoB == 'on') {      //Si el valor capturado en la variable antes declarada es "on" se realiza el código dentro de llaves.
+                    
+                var TipoRespuesta = $("#B").attr("class"); //Se captura, en la variable, la class de la pregunta correspondiente en el XML.
+                    
+                if (TipoRespuesta == "Correcta") { //Si el valor capturado corresponde a respuesta "correcta" se incrementa en 1 el contador de respuestas.
+                    ContadorCorrectas++;
+                };
+            }; 
+                                
+            if (resultadoC == 'on') {     //Si el valor capturado en la variable antes declarada es "on" se realiza el código dentro de llaves.
+                    
+                var TipoRespuesta = $("#C").attr("class"); //Se captura, en la variable, la class de la pregunta correspondiente en el XML.
+                    
+                if (TipoRespuesta == "Correcta") { //Si el valor capturado corresponde a respuesta "correcta" se incrementa en 1 el contador de respuestas.
+                    ContadorCorrectas++;
+                };
+            };    
+        };
         alert(ContadorCorrectas) //Saca un mensaje dealerta con las respuestas que has acertado.        
-        });
-    };    
+    });
 });
 
 
